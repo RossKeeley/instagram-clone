ab722ae51c8be21d179c3ef7324759543b9579c3
Ross Keeley <rjkeeley@gmail.com>
2021-03-11 19:18:04 +0000

    Added material ui and avatar to post card

--- instagram-clone/src/post.js
+++ instagram-clone/src/post.js
@@ -1,18 +1,26 @@
- import React from 'react';
- import './post.css';
+import React from 'react';
+import './post.css';
+import Avatar from '@material-ui/core/Avatar';
 
- function Post() {
-   return (
-     <div class="post">
+function Post() {
+ return (
+   <div className="post">
+     <div className="post_header">
+       <Avatar
+         className="post_avatar"
+         alt="Rosskeeley"
+         src="/static/images/avatar/1.jpg"
+       />
        <h3>Username</h3>
-       {/* header - avatar + username */}
-
-       {/* image */}
-       <img className="post_image" src="https://img.rasset.ie/0011c1e4-500.jpg" alt="" />
-       {/* username + caption */}
-       <h4>mrrosskeeley: Making an instagram clone! (image is not right lol)</h4>
      </div>
-   )
- }
+     {/* header - avatar + username */}
 
- export default Post
+     {/* image */}
+     <img className="post_image" src="https://img.rasset.ie/0011c1e4-500.jpg" alt="" />
+     {/* username + caption */}
+     <h4 className="post_text"><strong>mrrosskeeley</strong> Making an instagram clone! (image is not right lol)</h4>
+   </div>
+ )
+}
+
+export default Post
