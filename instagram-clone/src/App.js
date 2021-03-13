c897081db751407b1048dd7a1981d11b96ab03fb
Ross Keeley <rjkeeley@gmail.com>
2021-03-13 20:09:32 +0000

    Added props for posts

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -4,7 +4,7 @@ import Post from './post';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
 
       {/* Header */}
       <div className="app_header">
@@ -14,7 +14,10 @@ function App() {
       <h1>Hello</h1>
 
       {/* Posts */}
-      <Post />
+      <Post username="mrrosskeeley" caption="Making and instagram clone!" imageUrl="https://img.rasset.ie/0011c1e4-500.jpg" />
+      <Post username="practicalpod" caption="brunch in the city" imageUrl="https://i.pinimg.com/originals/f1/05/cd/f105cd1d6909c23823762dcad36612ab.jpg" />
+      <Post username="duckymcduckface" caption="I'm so big!!" imageUrl="https://www.dailybreeze.com/wp-content/uploads/migration/2014/201408/NEWS_140819552_AR_0_GOLPJJIEGDOP.jpg" />
+      <Post username="borisjohnson" caption="Well yes um there's eh an um little bit of a uh problem with the uh going outside um thing, and the-the-the staying indoors um thing as well" imageUrl="https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F973719a4-f2ee-11ea-9de6-a6e4d4016fb7.jpg?crop=3913%2C2201%2C65%2C381&resize=1180" />
       {/* Posts */}
       {/*  */}
 
--- instagram-clone/src/post.js
+++ instagram-clone/src/post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
  return (
    <div className="post">
      <div className="post_header">
@@ -11,14 +11,14 @@ function Post() {
          alt="Rosskeeley"
          src="/static/images/avatar/1.jpg"
        />
-       <h3>Username</h3>
+       <h3>{username}</h3>
      </div>
      {/* header - avatar + username */}
 
      {/* image */}
-     <img className="post_image" src="https://img.rasset.ie/0011c1e4-500.jpg" alt="" />
+     <img className="post_image" src={imageUrl} alt="" />
      {/* username + caption */}
-     <h4 className="post_text"><strong>mrrosskeeley</strong> Making an instagram clone! (image is not right lol)</h4>
+     <h4 className="post_text"><strong>{username}</strong> {caption}</h4>
    </div>
  )
 }
